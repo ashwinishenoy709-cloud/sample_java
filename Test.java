eafd88d4e8ec671e5bdcbe4977ebd30ed78b56a0
Ashwini Shenoy <ashwinishenoy709@gmail.com>
2026-07-22 14:39:24 +0530

    Change division to exponentiation in add method

--- Test.java
+++ Test.java
@@ -4,8 +4,8 @@ public class Test {
 
     int a=578;
     int b=459;
-    int result = a/b;
-    System.out.println("The Quotient is " +result);
+    int result = a**b;
+    System.out.println("The power is " +result);
     
   }
   public static void main(String[] args)
